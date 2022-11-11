751b2ecaf9a12f67139e369b7d1358038ae02b41
aschkun <82089320+aschkun@users.noreply.github.com>
2022-11-12 04:39:16 +0530

    Create colors.dart

---
+++ lib/widget/colors.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+
+class AppColors{
+  static final Color textColor1= Color(0xFF989acd);
+  static final Color textColor2 = Color(0xFF878593);
+  static final Color bigTextColor=Color(0xFF2e2e31);
+  static final Color mainColor= Color(0xFF5d69b3);
+  static final Color starColor = Color(0xFFe7bb4e);
+  static final Color mainTextColor = Color(0xFFababad);
+  static final Color buttonBackground = Color(0xFFf1f1f9);
+  static final Color greenColor = Color(0xFFF0FF42);
+  static final Color green1Color = Color(0xFF82CD47);
+  static final Color green2Color = Color(0xFF54B435);
+  static final Color green3Color = Color(0xFF379237);
+
+
+
+
+
+}
